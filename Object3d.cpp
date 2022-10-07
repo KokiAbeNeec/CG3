ed4dd04d5b88ac1e68592cb6b919758e11f64a43
LC1A_03_KokiAbe <k020g1182@g.neec.ac.jp>
2022-10-07 11:26:48 +0900

    01_06

--- Object3d.cpp
+++ Object3d.cpp
@@ -32,6 +32,8 @@ D3D12_VERTEX_BUFFER_VIEW Object3d::vbView{};
 D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
 Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
 unsigned short Object3d::indices[indexCount];
+XMMATRIX Object3d::matBillboard = XMMatrixIdentity();
+XMMATRIX Object3d::matBillboardY = XMMatrixIdentity();
 
 void Object3d::StaticInitialize(ID3D12Device * device, int window_width, int window_height)
 {
@@ -658,6 +660,31 @@ void Object3d::UpdateViewMatrix()
 
 	// ビュー行列に平行移動成分を設定
 	matView.r[3] = translation;
+
+#pragma region 全方向ビルボード行列の計算
+	// ビルボード行列
+	matBillboard.r[0] = cameraAxisX;
+	matBillboard.r[1] = cameraAxisY;
+	matBillboard.r[2] = cameraAxisZ;
+	matBillboard.r[3] = XMVectorSet(0, 0, 0, 1);
+#pragma region
+
+#pragma region Y軸回りビルボード行列の計算
+	// カメラX軸、Y軸、Z軸
+	XMVECTOR ybillCameraAxisX, ybillCameraAxisY, ybillCameraAxisZ;
+
+	// X軸は共通
+	ybillCameraAxisX = cameraAxisX;
+	ybillCameraAxisY = XMVector3Normalize(upVector);
+	ybillCameraAxisZ = XMVector3Cross(
+		cameraAxisX, cameraAxisY);
+
+	// Y軸回りビルボード行列
+	matBillboardY.r[0] = ybillCameraAxisX;
+	matBillboardY.r[1] = ybillCameraAxisY;
+	matBillboardY.r[2] = ybillCameraAxisZ;
+	matBillboardY.r[3] = XMVectorSet(0, 0, 0, 1);
+#pragma region
 }
 
 bool Object3d::Initialize()
@@ -698,6 +725,9 @@ void Object3d::Update()
 
 	// ワールド行列の合成
 	matWorld = XMMatrixIdentity(); // 変形をリセット
+
+	matWorld *= matBillboardY;	// ビルボード行列を掛ける
+
 	matWorld *= matScale; // ワールド行列にスケーリングを反映
 	matWorld *= matRot; // ワールド行列に回転を反映
 	matWorld *= matTrans; // ワールド行列に平行移動を反映
