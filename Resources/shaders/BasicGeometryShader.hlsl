0a817d83a6205fa8ecce7f975599f955bf02cbde
LC1A_03_KokiAbe <k020g1182@g.neec.ac.jp>
2022-10-14 11:15:16 +0900

    02_03

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -2,14 +2,21 @@
 
 [maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
+	GSOutput element;
+
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+
+	element.svpos = input[0].svpos;
+	output.Append(element);
+
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
+	output.Append(element);
+
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 }
